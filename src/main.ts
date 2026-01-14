37946f953304625c5a18731f0785566d5f9098ef
Cheems-code <osmar.ancco@tecsup.edu.pe>
2026-01-14 13:37:13 -0500

    chore: initial Nestjs project setup

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true })
+  );
+
+  const config = new DocumentBuilder()
+  .setTitle('Backend Core API')
+  .setDescription('API principal del sistema')
+  .setVersion('1.0')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+bootstrap();
