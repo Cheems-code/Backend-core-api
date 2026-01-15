aee451b43b3110d4bead3670b38dc679b85b9782
Cheems-code <osmar.ancco@tecsup.edu.pe>
2026-01-15 15:49:03 -0500

    refactor: restructure authentication module and move files to new directory

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
-import { PrismaModule } from '../prisma/prisma.module';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 const jwtSecret = process.env.JWT_SECRET;
 const expiresInRaw = process.env.JWT_EXPIRES_IN;
